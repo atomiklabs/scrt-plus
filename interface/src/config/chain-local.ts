93d3a73911d1481b244c5543f1de1587bb8338d4
Tomasz Kopacki <tomasz@kopacki.net>
2022-06-08 11:15:36 +0000

    fix(local-secret): uptdate local chain

--- interface/src/config/chain-local.ts
+++ interface/src/config/chain-local.ts
@@ -5,6 +5,6 @@ export const CONFIG = {
   CHAIN_gRPC: process.env.REACT_APP_CHAIN_GRPC,
   CHAIN_RPC: process.env.REACT_APP_CHAIN_RPC,
   CHAIN_REST: process.env.REACT_APP_CHAIN_REST,
-  contractAddr: 'secret10pyejy66429refv3g35g2t7am0was7ya6hvrzf',
-  contractHash: '0c99c7953f836f4573cca3b83dedb2a218acbf28a4702227211f4ad740df5c67',
+  contractAddr: 'secret1dqrzwx9trx3uhx5k6cm7dxm3dfgmsy58aq78qy',
+  contractHash: '2586f75d246bd6eba6cd68d092b74c5cd6dce2728e556452723ac0fa6b00dfad',
 }
--- interface/src/contract/query.ts
+++ interface/src/contract/query.ts
@@ -11,21 +11,22 @@ export async function getCount(
 
   console.log('counterAddress', contractAddress)
   console.log('counterHash', contractHash)
-  // console.log('secretjs', secretjs)
 
-  // TODO Fix:  Request failed with status code 404
-  // const countResponse = (await secretjs.query.compute.queryContract({
-  //   contractAddress: contractAddress,
-  //   codeHash: contractHash,
-  //   query: { get_count: {} },
-  // })) as CountResponse
+  try {
+    // TODO Fix:  Request failed with status code 404
+    const countResponse = (await secretjs.query.compute.queryContract({
+      contractAddress: contractAddress,
+      codeHash: contractHash,
+      query: { get_count: {} },
+    })) as CountResponse
+    
+    if ('err"' in countResponse) {
+      throw new Error(`Query failed with the following err: ${JSON.stringify(countResponse)}`)
+    }
 
-  const countResponse = await secretjs.query.compute.queryContract({ contractAddress, query: {} });
-
-  if ('err"' in countResponse) {
-    throw new Error(`Query failed with the following err: ${JSON.stringify(countResponse)}`)
+    return countResponse.count;
+  } catch (error) {
+    console.error(error)
+    return -1
   }
-  console.log({countResponse})
-
-  return 3 // countResponse.count
 }
